040afa1297c36c7a36351fd0f42a056dffdec03b
Andreas Hocevar <andreas.hocevar@gmail.com>
2021-06-15 12:59:09 +0200

    Updates for new API

--- src/geopackage.js
+++ src/geopackage.js
@@ -1,7 +1,12 @@
-import { open, getTileFromXYZ } from "@ngageoint/geopackage";
+import { GeoPackageAPI, GeoPackageTileRetriever } from "@ngageoint/geopackage";
 import TileLayer from "ol/layer/Tile";
+import VectorLayer from "ol/layer/Vector";
 import XYZ from "ol/source/XYZ";
+import VectorSource from "ol/source/Vector";
+import GeoJSON from "ol/format/GeoJSON";
+import {createEmpty, extend} from "ol/extent";
 import { map } from "./map";
+import { transformGeometryWithOptions } from "ol/format/Feature";
 
 export const loadGeoPackage = function (e) {
   var f = e.target.files[0];
@@ -13,11 +18,15 @@ export const loadGeoPackage = function (e) {
   r.readAsArrayBuffer(f);
 };
 
-function loadByteArray(array, callback) {
-  open(array, function (err, geoPackage) {
-    const tileTableNames = geoPackage.getTileTables();
-    const tileDao = geoPackage.getTileDao(tileTableNames[0]);
-    const tableLayer = new TileLayer({
+async function loadByteArray(array, callback) {
+  const geoPackage = await GeoPackageAPI.open(array);
+  const fitExtent = createEmpty()
+
+  const tileTableNames = await geoPackage.getTileTables();
+  tileTableNames.forEach(tileTableName => {
+    const tileDao = geoPackage.getTileDao(tileTableName);
+    const gpr = new GeoPackageTileRetriever(tileDao, 256, 256);
+    const tileLayer = new TileLayer({
       extent: [
         tileDao.tileMatrixSet.min_x,
         tileDao.tileMatrixSet.min_y,
@@ -31,15 +40,33 @@ function loadByteArray(array, callback) {
         url: "{z},{x},{y}",
         tileLoadFunction(tile, src) {
           const [z, x, y] = src.split(",").map(Number);
-          getTileFromXYZ(geoPackage, tileTableNames[0], x, y, z, 256, 256).then(
-            (dataUri) => {
-              tile.getImage().src = dataUri;
-            }
-          );
+          gpr.getTile(x, y, z).then(dataUri => tile.getImage().src = dataUri);
         },
       }),
     });
-    map.getView().fit(tableLayer.getExtent());
-    map.addLayer(tableLayer);
+    map.getView().fit(extend(fitExtent, tileLayer.getExtent()));
+    map.addLayer(tileLayer);
   });
+
+  const featureTableNames = await geoPackage.getFeatureTables();
+  featureTableNames.forEach(featureTableName => {
+    const featureDao = geoPackage.getFeatureDao(featureTableName);
+    const geojson = {
+      type: 'FeatureCollection',
+      features: []
+    }
+    const iterator = featureDao.queryForGeoJSONIndexedFeaturesWithBoundingBox(undefined, true);
+    for (const feature of iterator) {
+      feature.type = 'Feature';
+      geojson.features.push(feature);
+    }
+    const vectorLayer = new VectorLayer({
+      source: new VectorSource({
+        features: new GeoJSON().readFeatures(geojson, {featureProjection: 'EPSG:3857'})
+      })
+    });
+    map.getView().fit(extend(fitExtent, vectorLayer.getSource().getExtent()));
+    map.addLayer(vectorLayer);
+  });
+
 }
